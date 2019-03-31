b36c1b381428bfb0404a047f218a747f93ac0c26
BaliBalo <balibalo@pause-geek.fr>
2019-03-31 22:39:43 +0100

    Add cors config

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 2148;
 
-app.get('/', (req, res) => res.sendFile(path.resolve('data/ranked.json')));
+app.use(cors());
+app.use('/client', express.static('client'));
+app.get('/', (req, res) => res.sendFile(path.resolve('client/index.html')));
+app.get('/ranked', (req, res) => res.sendFile(path.resolve('data/ranked.json')));
+app.get('/peepee', (req, res) => res.sendFile(path.resolve('client/peepee.html')));
 
 app.listen(port, () => console.log('Scoresaber server listening (port '+port+')'));
