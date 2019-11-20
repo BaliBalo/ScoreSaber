3f50b80162973441c8d01e4a2ee185327c796691
BaliBalo <balibalo@pause-geek.fr>
2019-11-20 04:40:11 +0000

    Tweak scoresaber requests

--- scraper/scoresaber.js
+++ scraper/scoresaber.js
@@ -11,7 +11,7 @@ async function _scoreSaberRequest(path, retries = 0) {
 		});
 	} catch(e) {
 		if (3 > retries) {
-			await wait(1000);
+			await wait(3000);
 			return _scoreSaberRequest(path, retries + 1);
 		} else {
 			throw e;
@@ -23,5 +23,5 @@ async function scoreSaberRequest(path) { return _scoreSaberRequest(path, 0); }
 module.exports = {
 	request: scoreSaberRequest,
 	ranked: page => scoreSaberRequest('/api.php?function=get-leaderboards&cat=3&ranked=1&limit=100&page=' + (+page || 1)),
-	recentRanks: page => scoreSaberRequest('/api.php?function=get-leaderboards&cat=1&ranked=1&limit=15&page=' + (+page || 1)),
+	recentRanks: page => scoreSaberRequest('/api.php?function=get-leaderboards&cat=1&ranked=1&limit=10&page=' + (+page || 1)),
 };
