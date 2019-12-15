8a43dea1daaa5088a4c3e616514a4a9805c4d2a1
BaliBalo <balibalo@pause-geek.fr>
2019-12-15 18:05:20 +0000

    compress script no work on windows :( use a js file

---
+++ compress.js
@@ -0,0 +1,10 @@
+const { spawn } = require('child_process');
+
+const script = spawn('bash', ['-c', 'for f in client/*.js; do if [ "$f" != "${f%.min.js}" ] ; then continue; fi; npx terser $f -o ${f%.js}.min.js; done']);
+
+script.stdout.on('data', data => console.log(data));
+script.stderr.on('data', data => console.error(data));
+script.on('close', code => {
+	console.log('Done - exit code:', code);
+	process.exit(code);
+});
